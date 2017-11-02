80eb5c1d663bb28d6f08df6a10a9635e7cd3fa40
tacigar <ig.lhound@gmail.com>
2017-11-02 13:48:44 +0900

    Add init

---
+++ init.lua
@@ -0,0 +1,6 @@
+local _M = {}
+
+_M.math   = require "dustbox.math"
+_M.string = require "dustbox.string"
+
+return _M
---
+++ string/init.lua
@@ -0,0 +1,7 @@
+local _M = {}
+
+function _M.charAt(str, index)
+	return string.sub(str, index, index)
+end
+
+return _M
